5b060e01a0342411ba25328a36b4a9762eac18b6
kzadrozny <kzadrozny@uchicago.edu>
2021-05-05 16:02:34 -0500

    Added certifications

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,14 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass'); // Requires the gulp-sass plugin
-var browserSync = require('browser-sync').create();
-// var bourbon = require("bourbon").includePaths;
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
+sass.compiler = require('node-sass');
 
-gulp.task('sass', function(){
-  return gulp.src('src/scss/**/*.scss') // Gets all files ending with .scss in src/scss and children dirs
-    .pipe(sass())
-    .pipe(gulp.dest('dist/css'))
-	.pipe(browserSync.reload({
-      stream: true
-    }))
-});
-
-gulp.task('watch', ['browserSync', 'sass'], function(){
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('*.html', browserSync.reload);
-})
-
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: './'
-    },
-  })
-})
-
-gulp.task("default", ["sass", "watch"], function() {
-});
+gulp.task('sass', function () {
+   return gulp.src('./sass/**/*.scss')
+   .pipe(concat('styles.scss'))
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('./dist/'));
+ });
